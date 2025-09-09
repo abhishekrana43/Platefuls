050b97f611707ea432a05291d04e0f35771d1aeb
Abhishek Rana <abhishekrana0743@gmail.com>
2025-09-09 21:16:35 +0530

    Update all

---
+++ Backend/src/app.js
@@ -0,0 +1,24 @@
+// create server
+require("dotenv").config();
+const express = require('express');
+const cookieParser = require("cookie-parser");
+//const { compare } = require('bcrypt');
+const authRoutes = require('./routes/auth.routes');
+const foodRoutes = require('./routes/food.routes');
+
+
+
+
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+app.use('/api/auth', authRoutes);
+app.use('/api/food', foodRoutes);
+
+module.exports = app;
